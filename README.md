d1b6633fe25c612770e0c7a036d8763d14ae7783
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-08-12 23:01:10 +0530

    Set up initial codebase

---
+++ README.md
@@ -0,0 +1,2 @@
+# Offline_MovieDB
+Source code for a minimalistic Flutter application that stores a user-created movie watchlist on local storage.
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies Database',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Movies Database',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: Text('Dummy Screen'),
+      ),
+    );
+  }
+}
