367e2278670c79811a922f12e48aadee68be4f57
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-08-14 00:29:46 +0530

    Initialized firebase code and refactored main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:movies_database/pages/homePage.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.white,
-  ));
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  //   statusBarColor: Colors.black,
+  // ));
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
       .then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  static const appTitle = 'Movies Database';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Movies Database',
+      title: appTitle,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        brightness: Brightness.light,
-        centerTitle: true,
-        elevation: 0,
-        title: Text(
-          'Movies Database',
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body: Center(
-        child: Text('Dummy Screen'),
-      ),
     );
   }
 }
