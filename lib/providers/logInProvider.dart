f01ad74ccbd5acc802f6e56b3624eebede16d733
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-08-14 00:34:41 +0530

    Added login provider codebase for auth and signIn

---
+++ lib/providers/logInProvider.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LogInProvider extends ChangeNotifier {
+  final logIn = GoogleSignIn();
+
+  bool _bufferStatus = false;
+  bool get isLoading => _bufferStatus;
+
+  set isLoading(bool val) {
+    _bufferStatus = val;
+    notifyListeners();
+  }
+
+  logUser() async {
+    isLoading = true;
+
+    final user = await logIn.signIn();
+    if (user == null) {
+      isLoading = false;
+      return;
+    } else {
+      final auth = await user.authentication;
+
+      final creds = GoogleAuthProvider.credential(
+          accessToken: auth.accessToken, idToken: auth.idToken);
+
+      await FirebaseAuth.instance.signInWithCredential(creds);
+
+      isLoading = false;
+    }
+  }
+
+  logOut() async {
+    await logIn.signOut();
+    FirebaseAuth.instance.signOut();
+  }
+}
