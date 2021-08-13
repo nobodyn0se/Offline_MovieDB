137e507d9428bd096b4be7586893f252959ed406
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-08-14 00:40:47 +0530

    Added initial code for SignIn Screen

---
+++ lib/screens/signInScreen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:movies_database/providers/logInProvider.dart';
+import 'package:provider/provider.dart';
+
+class SignInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        brightness: Brightness.dark,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'Get Started',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 10),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.movie_creation,
+                  color: Colors.white,
+                  size: 48,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Text(
+                    'Offline Movies Watchlist',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 28,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(
+            flex: 3,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Sign in with your Google Account to create or edit your watchlist',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          Spacer(
+            flex: 5,
+          ),
+        ],
+      ),
+    );
+  }
+}
