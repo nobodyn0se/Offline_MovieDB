a01fd6c4bf35850404ef0c2d2c86d2ad7d8ab4c5
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-08-15 18:41:25 +0530

    Database table schema defined

--- lib/dbmodel/dbModel.dart
+++ lib/dbmodel/dbModel.dart
@@ -17,3 +17,38 @@ class MovieTable {
   static final String director = 'director';
   static final String time = 'time';
 }
+
+class Movies {
+  final int? id;
+  final bool isWatched;
+  final String imagePath;
+  final String movieName;
+  final String director;
+  final DateTime time;
+
+  Movies(
+      {this.id,
+      required this.isWatched,
+      required this.imagePath,
+      required this.movieName,
+      required this.director,
+      required this.time});
+
+
+  static Movies fromJson(Map<String, Object?> json) => Movies(
+      id: json[MovieTable.id] as int?,
+      isWatched: json[MovieTable.isWatched] == 1,
+      imagePath: json[MovieTable.imagePath] as String,
+      movieName: json[MovieTable.movieName] as String,
+      director: json[MovieTable.director] as String,
+      time: DateTime.parse(json[MovieTable.time] as String));
+
+  Map<String, Object?> toJson() => {
+        MovieTable.id: id,
+        MovieTable.isWatched: isWatched ? 1 : 0,
+        MovieTable.imagePath: imagePath,
+        MovieTable.movieName: movieName,
+        MovieTable.director: director,
+        MovieTable.time: time.toIso8601String(),
+      };
+}
